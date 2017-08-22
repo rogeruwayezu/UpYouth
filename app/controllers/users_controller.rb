b9e644143f38e9c722aa284387dae18b6bc22453
rogeruwayezu <rogeruwayezu@gmail.com>
2017-08-22 09:30:38 +0200

    added views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    
-
+    user = User.new (
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+        )
+    if user.save
+         session[:user_id] = user.id
+         flash[:success] = 'Successfully created account!'
+         redirect_to '/'
+       else
+         flash[:warning] = 'Invalid email or password!'
+         redirect_to '/signup'
+    end
   end
 end
