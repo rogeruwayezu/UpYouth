3dd90138868ef00877c2c2826e68e41878ccb5c8
Gedeon Ntwali <ntwari28@gmail.com>
2017-08-22 19:37:13 +0200

    working on post controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get "/" => "homepages#homepage"
 
   get "/posts", to: 'posts#index'
+  get "/posts/new", to: 'posts#new'
+  get "/posts/:id", to: 'posts#show'
+  post "/posts", to: 'posts#create'
+  get "/posts/:id/edit", to: 'posts#edit'
+  patch "/posts/:id", to: 'posts#update'
+  delete "/posts/:id", to: 'posts#destroy'
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
