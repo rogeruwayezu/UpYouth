82c57ce8a3c954c27d34c782ee9ec2dd6ec9a0c9
rogeruwayezu <rogeruwayezu@gmail.com>
2017-08-22 10:14:09 +0200

    completed users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def authenticate_employer!
+    unless current_user && current_user.user_employer
+      flash[:danger] = "Access Denied"
+      redirect_to "/"
+    end
+  end
+  
+  def authenticate_freelancer!
+    unless current_user && current_user.user_freelancer
+      flash[:danger] = "Access Denied"
+      redirect_to "/"
+    end
+  end
 end
